c8ff289c00bb69c2e1cd7856d2fea17b7fb8eb1f
vinicius marques <viniciusmc486@gmail.com>
2025-08-19 10:08:06 -0300

    Update contact app admin configuration and models; change project language and timezone settings

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone'
+    ordering = '-id',
+    list_per_page = 10
+    list_display_links = 'id', 'phone'
