1a21ee74fd59d5ddd577c0564602b53bfd3e83de
vinicius marques <viniciusmc486@gmail.com>
2025-08-27 16:33:09 -0300

    Refactor base template structure: create partials for head and header, update index template for better content handling, and implement search functionality in views.

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, contact
+from .views import index, contact, search
 
 app_name = 'contact'
 
 urlpatterns = [
     path('<int:contact_id>/', contact, name='contact'),
+    path('search/', search, name='search'),
     path('', index, name='index'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
+
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value
     }
 
     return render(
@@ -15,11 +46,15 @@ def index(request):
 
 
 def contact(request, contact_id):
-    single_contact = Contact.objects.get(pk=contact_id)
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
-
     return render(
         request,
         'contact/contact.html',
