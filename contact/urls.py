1c0f3281c283d8da3ba3cd1dd8674dec96ae9a9e
vinicius marques <viniciusmc486@gmail.com>
2025-09-05 16:51:30 -0300

    Implement contact deletion functionality: add delete view, update URLs, and enhance contact template with update and delete options.

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, contact, search, create, update
+from .views import index, contact, search, create, update, delete
 
 app_name = 'contact'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('contact/<int:contact_id>/', contact, name='contact'),
     path('contact/create/', create, name='create'),
     path('contact/<int:contact_id>/update/', update, name='update'),
+    path('contact/<int:contact_id>/delete/', delete, name='delete'),
 ]
